be8b88f082e571e647ffeb1fdfb4992f02dee21b
PrincewillIroka <princewilliroka@outlook.com>
2025-06-16 02:18:50 -0400

    Refactored User class for improved readability and consistency. Adjusted formatting by adding spaces around operators and comments for clarity. Updated Javadoc comments to enhance documentation quality. No functional changes were made.

--- src/midtermreviewcodeforpartc/User.java
+++ src/midtermreviewcodeforpartc/User.java
@@ -1,31 +1,31 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the.
  */
 package midtermreviewcodeforpartc;
 
 /**
  * A class that models a User for Uno Online.
  * The user has a name and a password.
+ * 
  * @author dancye, 2019
  */
-public class User 
-{
+public class User {
     private String name;
     private String password;
-    
+
     /**
      * a constructor that takes in the user's name and password
+     * 
      * @param givenName
-     * @param givenPass 
+     * @param givenPass
      */
-    public User(String givenName, String givenPass)
-    {
+    public User(String givenName, String givenPass) {
         name = givenName;
-        password=givenPass;
+        password = givenPass;
     }
-    
+
     public User(String name, String password, PasswordValidator validator) {
         this.name = name;
         if (validator.validate(password)) {
@@ -35,7 +35,9 @@ public class User
         }
     }
 
-    /** The getter for the user name
+    /**
+     * The getter for the user name
+     * 
      * @return the name
      */
     public String getName() {
@@ -44,6 +46,7 @@ public class User
 
     /**
      * The setter for the user name
+     * 
      * @param givenName the name to set
      */
     public void setName(String givenName) {
@@ -52,19 +55,20 @@ public class User
 
     /**
      * The getter for the password
+     * 
      * @return the password
      */
     public String getPassword() {
         return password;
     }
-    
+
     /**
      * A setter for the password
-     * @param givenPass 
+     * 
+     * @param givenPass
      */
-    public void setPassword(String givenPass)
-    {
+    public void setPassword(String givenPass) {
         password = givenPass;
     }
-    
+
 }
